99e3a29c5aea4c4868d6a1b581bd9bde8facd39d
jasonenkristo123 <jasonenkristo@gmail.com>
2026-05-13 14:15:47 +0700

    review binary and buble sort

--- src/SearchSort.java
+++ src/SearchSort.java
@@ -19,7 +19,7 @@ public class SearchSort {
 
 class BinarySearch {
     public static void main(String[] args) {
-        int [] arrays = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int [] arrays = {10, 4, 540, 3, 5, 9};
         int target = 5;
         int result = binarySearching(arrays, target);
         System.out.println(result);
@@ -40,6 +40,46 @@ class BinarySearch {
             } else if (target < arr[mid]) {
                 right = mid - 1;
             } else  {
+                left = mid + 1;
+            }
+        }
+        return index;
+    }
+}
+
+
+class SortNSearch {
+    public static void main(String[] args) {
+        
+    }
+
+    static int[] bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        int index = -1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == target) {
+                index = mid;
+                break;
+            } else if (target < arr[mid]) {
+                right = mid - 1;
+            } else {
                 left = mid + 1;
             }
         }
